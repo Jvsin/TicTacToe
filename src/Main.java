bf50d3498df18cc359e054c966ef51a68c29db5d
Piotr Jasiński <piotr.jasinski2002@gmail.com>
2023-05-27 11:43:54 +0200

    2. widok planszy i dodanie metody Start

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         new TicTacToe();
+//        Random rand = new Random();
+//        System.out.println(rand.nextInt(2));
 
     }
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -10,11 +10,24 @@ public class TicTacToe implements ActionListener {
     JLabel textInfo = new JLabel();
     JButton[] boardButton = new JButton[9];
 
+    int moves = 0;
+    Random rand = new Random();
+    int whoStart;
+    int playerFlag;
+
     TicTacToe() {
         mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainWindow.setSize(1000,1000);
         mainWindow.getContentPane().setBackground(new Color(255,255,255));
         mainWindow.setVisible(true);
+
+        infoPanel.setLayout(new BorderLayout());
+        infoPanel.setBounds(0,0,1000,200);
+
+        textInfo.setBackground(new Color(255,255,255));
+        textInfo.setForeground(new Color(255,0,0));
+        textInfo.setHorizontalAlignment(JLabel.CENTER);
+        textInfo.setFont(new Font( "Arial",Font.BOLD,50));
 
         gameBoard.setLayout(new GridLayout(3,3));
 
@@ -22,13 +35,31 @@ public class TicTacToe implements ActionListener {
             boardButton[i] = new JButton();
             gameBoard.add(boardButton[i]);
             boardButton[i].addActionListener(this);
+            boardButton[i].setBackground(new Color(255, 255, 255));
         }
 
+        infoPanel.add(textInfo);
+        mainWindow.add(infoPanel, BorderLayout.NORTH);
         mainWindow.add(gameBoard);
+
+        Start();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public void Start(){
+        whoStart = rand.nextInt(2);
+
+        if(whoStart == 0){
+            playerFlag = 0;
+            textInfo.setText("ZACZYNA X");
+        }
+        else {
+            playerFlag = 1;
+            textInfo.setText("ZACZYNA O");
+        }
     }
 }
